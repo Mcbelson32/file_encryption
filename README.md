7b234f3f020bba7c37e59294f9d3d399d13792fc
Makbelhailu <mcwelson92@gmail.com>
2024-05-29 00:23:34 +0300

    feat(file): a litte custom and edit the README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # file_encryption
+
+
+[*] Encrypt any file
+[*] Decrypr any file
+[*] No need for private/public ke, only password
+[*] include File Navigation system(only in the current directory)
--- encdec.py
+++ encdec.py
@@ -11,6 +11,7 @@ def encrypt_file(file_path, encryption_key):
     with open(file_path + '.enc', 'wb') as encrypted_file:
         encrypted_file.write(encrypted_data)
 
+
 def decrypt_file(file_path, encryption_key):
     with open(file_path, 'rb') as encrypted_file:
         encrypted_data = encrypted_file.read()
@@ -22,26 +23,39 @@ def decrypt_file(file_path, encryption_key):
     with open(file_path[:-4], 'wb') as decrypted_file:
         decrypted_file.write(decrypted_data)
 
+
 def main():
     print("Files in the current directory:")
     for i, file in enumerate(os.listdir()):
         print(f"\t{i+1}. {file}")
+    
+    while True:
+        try :
+            file_choice = int(input("Enter the number of the file to encrypt or decrypt: ")) - 1
+            file_path = os.listdir()[file_choice]
+            break
+        except Exception :
+            print("Enter the correct number of the file!")
+            pass
+    while True:
+        operation = input("Do you want to (E)ncrypt,  (D)ecrypt, (A)bort? ")
+    
+        if operation.upper() == "E":
+            encryption_key = input("Enter the encryption key: ")
+            encrypt_file(file_path, encryption_key)
+            print("File encrypted successfully! \n\t Password: " + encryption_key)
+            break
+        elif operation.upper() == "D":
+            encryption_key = input("Enter the decryption key: ")
+            decrypt_file(file_path, encryption_key)
+            print("File decrypted successfully!")
+            break
+        elif operation.upper() == "A" :
+            print("operation has been aborted.")
+            break
+        else:
+            print("Invalid operation. Please enter 'E' to encrypt, 'D' to decrypt or 'A' to abort.")
 
-    file_choice = int(input("Enter the number of the file to encrypt or decrypt: ")) - 1
-    file_path = os.listdir()[file_choice]
-
-    operation = input("Do you want to (E)ncrypt or (D)ecrypt? ")
-
-    if operation.upper() == "E":
-        encryption_key = input("Enter the encryption key: ")
-        encrypt_file(file_path, encryption_key)
-        print("File encrypted successfully! \n\t Password: " + encryption_key)
-    elif operation.upper() == "D":
-        encryption_key = input("Enter the decryption key: ")
-        decrypt_file(file_path, encryption_key)
-        print("File decrypted successfully!")
-    else:
-        print("Invalid operation. Please enter 'E' to encrypt or 'D' to decrypt.")
 
 if __name__ == "__main__":
     main()
